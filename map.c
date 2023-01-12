19abe627b6ed7fd70d948a12da9780c78dc6d13a
cosmina2902 <cosmina.recea@student.upt.ro>
2023-01-12 09:49:47 +0200

    Am adaugat varianta finala

--- map.c
+++ map.c
@@ -1,89 +1,63 @@
+#include <stdio.h>
+#include <stdbool.h>
 
 #define N 8
-#include <stdbool.h>
-#include <stdio.h>
+
+int board[N][N];
+int solutions;
+
+bool isValidMove(int row, int col) {
+    
+    for (int i = 0; i < row; i++) {
+        if (board[i][col]) {
+            return false;
+        }
+    }
 
 
-void printSolution(int board[N][N])
-{
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++)
-			printf(" %d ", board[i][j]);
-		printf("\n");
-	}
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+        if (board[i][j]) {
+            return false;
+        }
+    }
+
+    for (int i = row, j = col; i >= 0 && j < N; i--, j++) {
+        if (board[i][j]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-
-bool isSafe(int board[N][N], int row, int col)
-{
-	int i, j;
-
-	
-	for (i = 0; i < col; i++)
-		if (board[row][i])
-			return false;
-
-	
-	for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
-		if (board[i][j])
-			return false;
-
-
-	for (i = row, j = col; j >= 0 && i < N; i++, j--)
-		if (board[i][j])
-			return false;
-
-	return true;
+void printBoard() {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
-bool solveNQUtil(int board[N][N], int col)
-{
-	
-	if (col >= N)
-		return true;
+void solve(int row) {
+    if (row == N) {
+        printBoard();
+        solutions++;
+        return;
+    }
 
-
-	for (int i = 0; i < N; i++) {
-		
-		if (isSafe(board, i, col)) {
-			
-			board[i][col] = 1;
-
-			
-			if (solveNQUtil(board, col + 1))
-				return true;
-
-		
-			board[i][col] = 0; 
-		}
-	}
-
-	
-	return false;
+    for (int i = 0; i < N; i++) {
+        if (isValidMove(row, i)) {
+            board[row][i] = 1;
+      
+	        solve(row + 1);
+            board[row][i] = 0;
+        }
+    }
 }
 
-
-bool solveNQ()
-{
-	int board[N][N] = { { 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 },
-						{ 0, 0, 0, 0 } };
-
-	if (solveNQUtil(board, 0) == false) {
-		printf("Solution does not exist");
-		return false;
-	}
-
-	printSolution(board);
-	return true;
+int main() {
+    solve(0);
+    return 0;
 }
-
-
-int main()
-{
-	solveNQ();
-	return 0;
-}
-
-
